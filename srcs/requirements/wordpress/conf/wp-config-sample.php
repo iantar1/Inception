f3fc443425ab20e4402c7f5ae48201e47b358194
iantar1 <antartalha@gmail.com>
2024-05-18 12:00:28 -0400

    ls

--- srcs/requirements/wordpress/conf/wp-config-sample.php
+++ srcs/requirements/wordpress/conf/wp-config-sample.php
@@ -34,6 +34,8 @@ define( 'DB_HOST', 'localhost' );
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
+
+
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
