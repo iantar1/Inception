937dd1f00aa6c5757be12575385d3da2bfe52762
iantar1 <antartalha@gmail.com>
2024-06-01 07:00:55 -0400

    wa khdaaaama

--- test/wordpress/wp_script.sh
+++ test/wordpress/wp_script.sh
@@ -5,6 +5,15 @@
 # --dbuser=iantar \
 # --dbpass=1234 \
 # --dbhost=mariadb:3306 --path='/var/www/wordpress'
+sleep 5
+
+wp core install --allow-root \
+	--title="Azul-flown" \
+	--admin_user=iantar \
+	--admin_password=1234 \
+	--admin_email=antartalha@gmail.com \
+	--url=localhost \
+	--path='/var/www/wordpress'
 
 sleep 2
 
