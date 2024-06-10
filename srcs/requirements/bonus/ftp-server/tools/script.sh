0a699eda8b448ebcca31b5abe79bd9e5328bc41a
ismail antar <antartalha@gmail.com>
2024-06-10 13:17:28 +0100

    cadviser , ftp done

--- srcs/requirements/bonus/ftp-server/tools/script.sh
+++ srcs/requirements/bonus/ftp-server/tools/script.sh
@@ -4,16 +4,14 @@ mkdir -p /var/run/vsftpd/empty
 
 mkdir -p /var/www/wordpress
 
-# create a new user without password and set all the user informations to empty string
-# to avoid promping when running (general electric comprehensive operating system)
-adduser iantar --disabled-password -gecos ""
+useradd -m iantar
+
 echo "iantar:1234" | chpasswd
-chown -R iantar:iantar /var/www/wordpress
+
+chown iantar /var/www/wordpress
 
 echo iantar >> /etc/vsftpd.user_list
 
-
-# sleep 5
-
 echo "FTP starting..."
+
 vsftpd /etc/vsftpd.conf
